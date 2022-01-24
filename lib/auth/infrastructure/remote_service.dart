bedf6535c89fd4d51fcfb6eb702876fa34444e56
Hasan Hallak <hasanm.hallak@gmail.com>
2022-01-24 16:23:26 -0500

    Added remote_service

---
+++ lib/auth/infrastructure/remote_service.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/domain/auth_failure.dart';
+
+import 'keys.dart';
+
+class RemoteService {
+  final authorizationEndpoint = Uri.parse('https://gitlab.com/oauth/authorize');
+
+  final tokenEndpoint = Uri.parse('https://gitlab.com/oauth/token');
+
+  final redirectedUrl = Uri.parse('https://localhost:3000/callback');
+
+  final scopes = ['read_user', 'read_repository'];
+
+  /// Creates a new grant.
+  AuthorizationCodeGrant grant() {
+    return AuthorizationCodeGrant(
+      identifier,
+      authorizationEndpoint,
+      tokenEndpoint,
+      secret: secret,
+    );
+  }
+
+  /// Returns the URL to which the resource owner should be redirected to authorize this client.
+  Uri getAuthorizationUrl(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectedUrl, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Credentials>> getCredentials(
+    AuthorizationCodeGrant grant,
+    Map<String, String> parameters,
+  ) async {
+    try {
+      final client = await grant.handleAuthorizationResponse(parameters);
+      return right(client.credentials);
+    } on FormatException catch (e) {
+      return left(AuthFailure.server(e.message));
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server('${e.error}: ${e.description}'));
+    }
+  }
+}
