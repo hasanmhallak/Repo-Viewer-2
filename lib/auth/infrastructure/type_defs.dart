b90504131a2b3ef4ab0bb6e70df39f2ac9c70c39
Hasan Hallak <hasanm.hallak@gmail.com>
2022-01-26 10:48:32 -0500

    Added docs to type_defs.dart

--- lib/auth/infrastructure/type_defs.dart
+++ lib/auth/infrastructure/type_defs.dart
@@ -1,2 +1,5 @@
-/// Handles the proccess of redirecting the user
+/// Handles the proccess of redirecting the resource owner
+/// to the authorization URL and getting back the URL that
+/// holds the code in the [queryParameters] in which will
+/// be exchanged for an accessToken.
 typedef AuthHandler = Future<Uri> Function(Uri authUri);
