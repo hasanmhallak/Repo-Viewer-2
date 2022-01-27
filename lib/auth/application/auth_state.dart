e07297dc354309e57b5bd280047d1b2a685e6b52
Hasan Hallak <hasanm.hallak@gmail.com>
2022-01-27 12:02:45 -0500

    Added auth_notifier and start implementing it.

---
+++ lib/auth/application/auth_state.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/auth/domain/entities/auth_failure.dart';
+
+part 'auth_state.freezed.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  const AuthState._();
+  const factory AuthState.initial() = _Initial;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
+  const factory AuthState.authenticated() = _Authenticated;
+  const factory AuthState.failure(AuthFailure failure) = _Failure;
+}
--- lib/auth/domain/entities/auth_failure.dart
+++ lib/auth/domain/entities/auth_failure.dart
@@ -2,7 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'auth_failure.freezed.dart';
 
-//@freezed
+@freezed
 class AuthFailure with _$AuthFailure {
   const AuthFailure._();
   const factory AuthFailure.storage([String? message]) = _Storage;
