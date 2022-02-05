890762fa49417249271f69818b4582ba7e7fc144
Hasan Hallak <hasanm.hallak@gmail.com>
2022-02-05 13:51:12 -0500

    Added a database

---
+++ lib/repos/core/infrastructure/database/database.dart
@@ -0,0 +1,34 @@
+import 'sembast_database.dart';
+
+abstract class DataBase {
+  factory DataBase(StoreType type, String name, SembastDatabase db) =
+      SembastDbStore;
+
+  /// Save multiple records, creating the one needed.
+  ///
+  /// The list of [values] must match the list of [keys].
+  Future<void> saveRecords(
+    List<dynamic> keys,
+    List<Map<String, dynamic>> values,
+  );
+
+  /// Save a record, create if needed.
+  Future<void> saveRecord(
+    dynamic key,
+    Map<String, dynamic> value,
+  );
+
+  /// Get a record value from the database.
+  Future<Map<String, dynamic>?> getRecord(dynamic key);
+
+  /// Get all records values.
+  Future<List<Map<String, dynamic>>> getRecords(List<dynamic> keys);
+
+  /// Find multiple records.
+  ///
+  /// Returns an empty array if none found.
+  Future<List<Map<String, dynamic>>> findRecords({
+    int? offset,
+    int? limit,
+  });
+}
---
+++ lib/repos/core/infrastructure/database/sembast_database.dart
@@ -0,0 +1,81 @@
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+
+import 'database.dart';
+
+enum StoreType { intMap, stringMap }
+
+class SembastDatabase {
+  late Database _instance;
+  Database get instance => _instance;
+  bool _hasBeenInitialized = false;
+
+  Future<void> init() async {
+    if (_hasBeenInitialized) {
+      return;
+    } else {
+      final directory = await getApplicationDocumentsDirectory();
+      final dbPath = join(directory.path, 'db.sembast');
+      _instance = await databaseFactoryIo.openDatabase(dbPath);
+      _hasBeenInitialized = true;
+    }
+  }
+}
+
+class SembastDbStore implements DataBase {
+  final SembastDatabase _db;
+  late StoreRef _store;
+  SembastDbStore(StoreType type, String name, this._db) {
+    switch (type) {
+      case StoreType.intMap:
+        _store = intMapStoreFactory.store(name);
+        break;
+      case StoreType.stringMap:
+        _store = stringMapStoreFactory.store(name);
+    }
+  }
+
+  @override
+  Future<void> saveRecords(
+    List<dynamic> keys,
+    List<Map<String, dynamic>> values,
+  ) async {
+    await _store.records(keys).put(_db.instance, values);
+  }
+
+  @override
+  Future<void> saveRecord(
+    dynamic key,
+    Map<String, dynamic> value,
+  ) async {
+    await _store.record(key).put(_db._instance, value);
+  }
+
+  @override
+  Future<Map<String, dynamic>?> getRecord(dynamic key) async {
+    return await _store.record(key).get(_db._instance) as Map<String, dynamic>?;
+  }
+
+  @override
+  Future<List<Map<String, dynamic>>> getRecords(List<dynamic> keys) async {
+    return await _store.records(keys).get(_db._instance)
+        as List<Map<String, dynamic>>;
+  }
+
+  @override
+  Future<List<Map<String, dynamic>>> findRecords({
+    int? offset,
+    int? limit,
+  }) async {
+    final records = await _store.find(
+      _db._instance,
+      finder: Finder(
+        offset: offset,
+        limit: limit,
+      ),
+    );
+    return records.map((e) => e.value as Map<String, dynamic>).toList();
+  }
+}
