297d94a2ba015cff63d968d36fca5e5e025c0996
Hasan Hallak <hasanm.hallak@gmail.com>
2022-02-15 10:47:50 -0500

    Starred working on the ui

--- lib/repos/starred_repo/presentation/starred_repos_page.dart
+++ lib/repos/starred_repo/presentation/starred_repos_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:repo_viewer/repos/starred_repo/providers/provider.dart';
+
 import '../bloc/starred_bloc.dart';
 
-class StarredReposPage extends StatelessWidget {
+class StarredReposPage extends ConsumerWidget {
   final Future<void> Function() signout;
   const StarredReposPage(this.signout, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
@@ -17,18 +20,18 @@ class StarredReposPage extends StatelessWidget {
           ),
         ],
       ),
-      body: BlocProvider(
-        create: (context) => StarredBloc(),
-        child: BlocConsumer<StarredEvent, StarredState>(
+      body: BlocProvider<StarredBloc>(
+        create: (context) => StarredBloc(ref.watch(starredRepositoryProvider)),
+        child: BlocConsumer<StarredBloc, StarredState>(
           listener: (context, state) {
             // TODO: implement listener
           },
           builder: (context, state) {
             return state.when(
-              initial: initial,
-              loading: loading,
-              loaded: loaded,
-              failure: failure,
+              initial: (freshRepos) => Container(),
+              loading: (freshRepos) => Container(),
+              loaded: (freshRepos) => Container(),
+              failure: (failure, freshRepos) => Container(),
             );
           },
         ),
