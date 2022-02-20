940c4ebefaea165743673d9bbf683af345aacd66
Hasan Hallak <hasanm.hallak@gmail.com>
2022-02-20 11:19:23 -0500

    Refactore notifier

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -1,4 +1,6 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:oauth2/oauth2.dart';
 
 import '../infrastructure/auth_repository.dart';
 import '../infrastructure/type_defs/type_defs.dart';
@@ -8,12 +10,23 @@ class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository _repository;
   AuthNotifier(this._repository) : super(const AuthState.initial());
 
+  Credentials? _credentials;
+
   /// Updates the `state` with user's status.
-  Future<void> checkAndUpdateAuthState() async {
-    final isSignedIn = await _repository.isSignedIn;
-    state = isSignedIn
-        ? const AuthState.authenticated()
-        : const AuthState.unauthenticated();
+  Future<Either<Credentials, Unit>> checkAndUpdateAuthState() async {
+    final credentialsOrFailure =
+        await _repository.getOrUpdateCredentials(_credentials);
+    state = credentialsOrFailure.fold(
+      (l) => const AuthState.unauthenticated(),
+      (r) {
+        _credentials = r;
+        return const AuthState.authenticated();
+      },
+    );
+    return credentialsOrFailure.fold(
+      (l) => right(unit),
+      (credentials) => left(credentials),
+    );
   }
 
   /// Signs the user in and updates the `state`.
@@ -31,7 +44,7 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   /// Signs the user out and updates the `state`.
   Future<void> signout() async {
-    final failureOrSuccess = await _repository.signout();
+    final failureOrSuccess = await _repository.signout(_credentials);
     state = failureOrSuccess.fold(
       (l) => const AuthState.unauthenticated(),
       (r) => const AuthState.authenticated(),
