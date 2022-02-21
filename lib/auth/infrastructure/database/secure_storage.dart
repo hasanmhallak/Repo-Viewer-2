98d10cea590abf845670d4c3f14c07fa2a8a5185
Hasan Hallak <hasanm.hallak@gmail.com>
2022-02-21 10:35:46 -0500

    Implement saving username logic

--- lib/auth/infrastructure/database/secure_storage.dart
+++ lib/auth/infrastructure/database/secure_storage.dart
@@ -5,14 +5,26 @@ import '../auth_local_service.dart';
 class SecureStorage implements AuthLocalService {
   const SecureStorage();
 
-  String get _key => 'credentials';
+  String get _credentialsKey => 'credentials';
+  String get _usernameKey => 'username';
   FlutterSecureStorage get _storage => const FlutterSecureStorage();
   @override
-  Future<void> delete() => _storage.delete(key: _key);
+  Future<void> deleteCredentials() => _storage.delete(key: _credentialsKey);
 
   @override
-  Future<String?> read() => _storage.read(key: _key);
+  Future<String?> readCredentials() => _storage.read(key: _credentialsKey);
 
   @override
-  Future<void> save(String data) => _storage.write(key: _key, value: data);
+  Future<void> saveCredentials(String credentials) =>
+      _storage.write(key: _credentialsKey, value: credentials);
+
+  @override
+  Future<void> deleteUsername() => _storage.delete(key: _usernameKey);
+
+  @override
+  Future<String?> readUsername() => _storage.read(key: _usernameKey);
+
+  @override
+  Future<void> saveUsername(String username) =>
+      _storage.write(key: _usernameKey, value: username);
 }
