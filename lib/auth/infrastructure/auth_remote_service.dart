d06b08222859b1c4712312b8d80481df407eb497
Hasan Hallak <hasanm.hallak@gmail.com>
2022-02-22 11:44:07 -0500

    Added getUserInfo method.
    The method getUserInfo will get the user information including
     `username` which we will use it to get the starred repos.

--- lib/auth/infrastructure/auth_remote_service.dart
+++ lib/auth/infrastructure/auth_remote_service.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/infrastructure/dtos/user_dto.dart';
 
 import '../../core/infrastructure/extinstions/dio_no_internet.dart';
 import 'keys.dart';
@@ -18,6 +20,8 @@ class AuthRemoteService {
   final _revokeEndpoint = Uri.parse('https://gitlab.com/oauth/revoke');
 
   final _scopes = ['read_user', 'read_repository'];
+
+  final _usernameUri = Uri.parse('https://gitlab.com/api/v4/user');
 
   /// Creates a new grant which uses HTTP Basic authentication as defined in RFC 2617.
   ///
@@ -99,10 +103,40 @@ class AuthRemoteService {
     } on DioError catch (e) {
       if (e.isNoConnection) {
         // token did not revoked.
+        // we still want the user to be able to signout even if there's no internet connection
+        // so nothing to do here except the fact that the app won't crash now.
         return;
       } else {
         // Unexpected Error.
-        // Let crash the app.
+        // Let it crash the app.
+        rethrow;
+      }
+    }
+  }
+
+  /// Can throw [AuthorizationException].
+  Future<Either<UserDTO, Unit>> getUserInfo(Credentials credentials) async {
+    try {
+      final response = await _dio.getUri(
+        _usernameUri,
+        options: Options(
+          headers: {'Authorization': 'bearer ${credentials.accessToken}'},
+        ),
+      );
+      if (response.statusCode == 200) {
+        final dto = UserDTO.fromJson(response.data as Map<String, dynamic>);
+        return left(dto);
+      } else {
+        throw AuthorizationException(
+          response.statusCode.toString(),
+          response.statusMessage,
+          null,
+        );
+      }
+    } on DioError catch (e) {
+      if (e.isNoConnection) {
+        return right(unit);
+      } else {
         rethrow;
       }
     }
