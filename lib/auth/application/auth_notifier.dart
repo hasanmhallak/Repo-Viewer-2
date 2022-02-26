f5c11f1182222d9ecbe89c02a24e0f53658d98b2
Hasan Hallak <hasanm.hallak@gmail.com>
2022-02-26 13:09:33 -0500

    Implements an interface and refactor

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -1,32 +1,31 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oauth2/oauth2.dart';
 
+import '../domain/entities/auth_failure.dart';
+import '../domain/entities/user.dart';
 import '../infrastructure/auth_repository.dart';
 import '../infrastructure/type_defs/type_defs.dart';
 import 'auth_state.dart';
+import 'notifier_interface.dart';
 
-class AuthNotifier extends StateNotifier<AuthState> {
+class AuthNotifier extends StateNotifier<AuthState> with Notifier {
   final AuthRepository _repository;
   AuthNotifier(this._repository) : super(const AuthState.initial());
 
   Credentials? _credentials;
+  User? _user;
+
+  void _handleFailure(AuthFailure failure) {
+    // Show a message.
+    state = AuthState.failure(failure);
+    state = const AuthState.unauthenticated();
+    _credentials = null;
+    _user = null;
+  }
 
   /// Updates the `state` with user's status.
-  Future<Either<Credentials, Unit>> checkAndUpdateAuthState() async {
-    final credentialsOrFailure =
-        await _repository.getOrUpdateCredentials(_credentials);
-    state = credentialsOrFailure.fold(
-      (l) => const AuthState.unauthenticated(),
-      (r) {
-        _credentials = r;
-        return const AuthState.authenticated();
-      },
-    );
-    return credentialsOrFailure.fold(
-      (l) => right(unit),
-      (credentials) => left(credentials),
-    );
+  Future<void> checkAndUpdateAuthState() async {
+    await _getUserInfoAndUpdateStateIfFailed();
   }
 
   /// Signs the user in and updates the `state`.
@@ -36,7 +35,8 @@ class AuthNotifier extends StateNotifier<AuthState> {
     );
     state = failureOrSuccess.fold(
       (failure) => AuthState.failure(failure),
-      (success) {
+      (credentials) {
+        _credentials = credentials;
         return const AuthState.authenticated();
       },
     );
@@ -49,5 +49,50 @@ class AuthNotifier extends StateNotifier<AuthState> {
       (l) => const AuthState.unauthenticated(),
       (r) => const AuthState.authenticated(),
     );
+  }
+
+  Future<void> _getCredentialsAndUpdateState() async {
+    final credentialsOrFailure =
+        await _repository.getOrUpdateCredentials(_credentials);
+    return credentialsOrFailure.fold(
+      (failure) {
+        _handleFailure(failure);
+      },
+      (credentials) {
+        state = const AuthState.authenticated();
+        _credentials = credentials;
+      },
+    );
+  }
+
+  /// Refreshes [Credentials] & [User].
+  Future<void> _getUserInfoAndUpdateStateIfFailed() async {
+    await _getCredentialsAndUpdateState();
+    if (_credentials == null) {
+      return;
+    } else {
+      final userOrFailure =
+          await _repository.getOrUpdateUserInfo(_credentials!);
+      return userOrFailure.fold(
+        (failure) {
+          _handleFailure(failure);
+        },
+        (user) {
+          _user = user;
+        },
+      );
+    }
+  }
+
+  @override
+  Future<Credentials?> get credentials async {
+    await checkAndUpdateAuthState();
+    return _credentials;
+  }
+
+  @override
+  Future<User?> get user async {
+    await checkAndUpdateAuthState();
+    return _user;
   }
 }
