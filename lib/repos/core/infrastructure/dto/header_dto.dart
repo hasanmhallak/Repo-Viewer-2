9438e25041fd51683743a916786120be69c54ed1
Hasan Hallak <hasanm.hallak@gmail.com>
2022-03-06 12:52:34 -0500

    Refactore and Fix header_DTO

--- lib/repos/core/infrastructure/dto/header_dto.dart
+++ lib/repos/core/infrastructure/dto/header_dto.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages, invalid_annotation_target
+// ignore_for_file: invalid_annotation_target
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -10,34 +10,21 @@ part 'header_dto.g.dart';
 //! [""];
 //! But the headers key may be not exist so we must provide a default value.
 
-String _stringFromJson(List<String> list) {
-  return list[0];
-}
-
-int _nextPageFromJson(List<String> list) {
-  final nextPageAsString = list[0];
-  final nextPageAsIntOrNull = int.tryParse(nextPageAsString);
-  return nextPageAsIntOrNull ?? 0;
-}
-
 @freezed
 class HeaderDTO with _$HeaderDTO {
   const HeaderDTO._();
   const factory HeaderDTO({
     @JsonKey(
       defaultValue: '',
-      fromJson: _stringFromJson,
     )
         required String url,
     @JsonKey(
       defaultValue: '',
-      fromJson: _stringFromJson,
       name: 'Etag',
     )
         required String etag,
     @JsonKey(
       defaultValue: 0,
-      fromJson: _nextPageFromJson,
       name: 'X-Next-Page',
     )
         required int nextPage,
@@ -47,7 +34,7 @@ class HeaderDTO with _$HeaderDTO {
 
   factory HeaderDTO.parse(String url, Map<String, List<String>> headers) {
     // checkimg if the key is not exist.
-    final nextPageAsString = headers['Etag']?[0] ?? '';
+    final nextPageAsString = headers['X-Next-Page']?[0] ?? '';
     final nextPageAsIntOrNull = int.tryParse(nextPageAsString);
     return HeaderDTO(
       url: url,
