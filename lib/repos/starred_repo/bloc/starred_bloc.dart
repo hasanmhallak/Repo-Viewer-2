0b055407ff711afcf2dd198139c0400e55ca62d4
Hasan Hallak <hasanm.hallak@gmail.com>
2022-03-06 13:46:41 -0500

    sorts import

--- lib/repos/starred_repo/bloc/starred_bloc.dart
+++ lib/repos/starred_repo/bloc/starred_bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:repo_viewer/repos/core/domain/fresh.dart';
 import 'package:repo_viewer/repos/core/infrastructure/pagination_config.dart';
 import 'package:repo_viewer/repos/starred_repo/domain/repo.dart';
-import 'package:repo_viewer/repos/starred_repo/domain/starred_repository.dart';
+import 'package:repo_viewer/repos/starred_repo/infrastructure/starred_repository.dart';
 
 import 'starred_event.dart';
 import 'starred_state.dart';
