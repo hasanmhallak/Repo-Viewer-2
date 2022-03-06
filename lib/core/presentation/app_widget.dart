19079fad7f9b2b7aa2000490fb1f115cdbde06fb
Hasan Hallak <hasanm.hallak@gmail.com>
2022-03-06 12:44:06 -0500

    Moved checkAndUpdateState to initializerProvider

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -4,7 +4,7 @@ import 'package:sizer/sizer.dart';
 
 import '../../auth/application/auth_state.dart';
 import '../../auth/providers/providers.dart';
-// import '../../repos/core/providers/provider.dart';
+import '../../repos/core/providers/provider.dart';
 import 'routes/app_router.dart';
 
 class AppWidget extends ConsumerWidget {
@@ -28,8 +28,8 @@ class AppWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef provider) {
-    // provider.listen(initializationProvider, (previous, next) {});
-    provider.read(authNotifier.notifier).checkAndUpdateAuthState();
+    provider.listen(initializationProvider, (previous, next) {});
+
     provider.listen<AuthState>(authNotifier, (previous, next) {
       next.maybeWhen(
         orElse: () {},
